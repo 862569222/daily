f192d24dc9aafffdf6066bb93b48dea2f095df5b
zhaibo <@zhaibo>
2023-12-22 17:19:26 +0800

    添加注释

--- algorithm/src/main/java/cn/daily/algorithm/code/code_05/快排.java
+++ algorithm/src/main/java/cn/daily/algorithm/code/code_05/快排.java
@@ -9,6 +9,11 @@ import java.util.Stack;
 public class 快排 {
     /**
      * 快排1。0
+     * Partition过程
+     *
+     * 给定一个数组arr，和一个整数num。请把小于等于num的数放在数组的左边，大于num的数放在数组的右边。
+     *
+     * 要求额外空间复杂度O(1)，时间复杂度O(N)
      * @param arr
      */
     public static void quickSort(int[] arr){
@@ -46,6 +51,12 @@ public class 快排 {
 
     /**
      * 随机快排 3。0
+     * 在arr[L..R]范围上，进行快速排序的过程：
+     * 1）在这个范围上，随机选一个数记为num，
+     * 1）用num对该范围做partition，< num的数在左部分，== num的数中间，>num的数在右部分。假设== num的数所在范围是[a,b]
+     * 2）对arr[L..a-1]进行快速排序(递归)
+     * 3）对arr[b+1..R]进行快速排序(递归)
+     * 因为每一次partition都会搞定一批数的位置且不会再变动，所以排序能完成
      * @param arr
      */
     public static void quickSort3(int[] arr){
@@ -86,7 +97,17 @@ public class 快排 {
         return left;
     }
 
-    //荷兰国旗问题
+    /**
+     * 荷兰国旗问题
+     *
+     * 给定一个数组arr，和一个整数num。请把小于num的数放在数组的左边，等于num的数放在中间，大于num的数放在数组的右边。
+     *
+     * 要求额外空间复杂度O(1)，时间复杂度O(N)
+     * @param arr
+     * @param l
+     * @param r
+     * @return
+     */
     //取arr[r]的值为划分值 <arr[r]  =arr[r]  >arr[r]，返回划分区域坐标
     // 2 0 1 4 2 8 2
     public static int[] netherlandsFlag(int[] arr ,int l ,int r ){
@@ -150,7 +171,7 @@ public class 快排 {
             }
         }
     }
- 
+
 
     public static void swap(int[] arr ,int l,int r){
         int temp = arr[l];
